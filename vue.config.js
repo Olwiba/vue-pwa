dbbfb4e2e0f838e1572b5ce96d01edca88f4e0e0
Ollie Bannister <olwiba@gmail.com>
2023-09-11 16:30:22 +1200

    It's a working PWA now

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    pwa: {
+        name: 'My Vue PWA',
+        themeColor: '#4DBA87', // Specifies the primary color for your app
+        appleMobileWebAppCapable: 'yes', // For iOS support
+        appleMobileWebAppStatusBarStyle: 'black',
+    }
+}
