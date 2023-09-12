229792badd34caf6e6e11dd01e549acf69dfe675
Ollie Bannister <olwiba@gmail.com>
2023-09-12 12:24:43 +1200

    Add a second error trigger and error reporting handler
    Added a new button `Trigger error two` that triggers a different error method. This new method called `triggerErrorTwo` throws a specified Error and catches it to send the error information with `rg4js`. This allows for the error to be manually logged, providing more control and visibility to the type and handling of errors in the application.

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -6,6 +6,15 @@ const triggerError = () => {
   throw new Error('This is an error');
 }
 
+const triggerErrorTwo = () => {
+  try {
+    throw new Error("Description of the error");
+  }
+  catch (e) {
+    rg4js('send', e);
+  }
+}
+
 const triggerNavigation = () => {
   const pages = ['about', 'team', 'settings']
   rg4js('trackEvent', {
@@ -24,6 +33,13 @@ const triggerNavigation = () => {
         @click="triggerError"
     >
       Trigger error
+    </button>
+    <button
+        type="button"
+        class="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        @click="triggerErrorTwo"
+    >
+      Trigger error two
     </button>
   </WelcomeItem>
 
